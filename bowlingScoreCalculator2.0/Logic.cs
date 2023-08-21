5793d563456ef939fce49d53647260038c92dfca
LambourE <LambourE@IT08570>
2023-08-21 10:05:57 +0100

    Add project files.

---
+++ bowlingScoreCalculator2.0/Logic.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bowlingScoreCalculator2._0
+{
+    internal class logic
+    {
+        public static int firstThrow()
+        {
+            while (true)
+            {
+                int pinsHit = Convert.ToInt32(Console.ReadLine());
+                if (pinsHit < 0 || pinsHit > 10)
+                {
+                    Console.WriteLine("Please enter a valid number (between 0-10)");
+                }
+                else
+                {
+                    return pinsHit;
+                }
+            }
+        }
+
+        public static int secondThrow()
+        {
+            while (true)
+            {
+                int pinsHit = Convert.ToInt32(Console.ReadLine());
+                if (pinsHit < 0 || pinsHit > 10)
+                {
+                    Console.WriteLine("Please enter a valid number (between 0-10)");
+                }
+                else
+                {
+                    return pinsHit;
+                }
+            }
+        }
+
+        public static int spareCheck(int i, int firstThrow, int secondThrow)
+        {
+            if (firstThrow + secondThrow == 10)
+            {
+                return i;
+            }
+            return 999;
+        }
+    }
+}
---
+++ bowlingScoreCalculator2.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using static bowlingScoreCalculator2._0.logic;
+
+var scores = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    int throwOne = firstThrow();
+    scores.Add(throwOne);
+
+    strikeCheck(throwOne);
+
+    int throwTwo = secondThrow();
+
+    int spare = spareCheck(i, throwOne, throwTwo);
+}
